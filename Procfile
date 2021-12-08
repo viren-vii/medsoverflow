488913590cb337bc9971223a7faebb8f1ef015ed
vire-vii <virubhosale112@gmail.com>
2021-12-08 17:07:51 +0530

    commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn medsoverflow.wsgi --log-file -
+
--- medsoverflow/settings.py
+++ medsoverflow/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['medsoverflow.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
