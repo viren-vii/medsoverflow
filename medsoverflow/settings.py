8d157be0fbf964f0fba2f543f194ebe60a284c97
vire-vii <virubhosale112@gmail.com>
2021-12-09 15:03:22 +0530

    Automatic configuration with django-heroku

--- medsoverflow/settings.py
+++ medsoverflow/settings.py
@@ -142,3 +142,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
